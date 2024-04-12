dfa99c91b0ad2aec486f2acebd40d49a7788b460
dkunhamb <dipin.kunhambunair@ansys.com>
2024-04-12 16:34:56 -0500

    modify bat file

--- src/ansys/mechanical/env/mechanical-env.bat
+++ src/ansys/mechanical/env/mechanical-env.bat
@@ -2,6 +2,7 @@
 if "%OS%"=="Windows_NT" (
     echo This script is intended for Linux environments only.
     echo Use without `mechanical-env` if you are using Windows
+    echo For more information:
+    echo https://mechanical.docs.pyansys.com/version/stable/getting_started/running_mechanical.html#embed-a-mechanical-instance
     exit /b 1
 )
-bash mechanical-env.sh %*
