2e26bc8542db07d69afcaa5d7f4f8ee2953a5382
dkunhamb <dipin.kunhambunair@ansys.com>
2024-04-15 14:31:21 -0500

    adapt new get_mechanical

--- src/ansys/mechanical/env/run.py
+++ src/ansys/mechanical/env/run.py
@@ -53,18 +53,10 @@ def cli_find_mechanical(version: int):
 
     >>> find-mechanical -r 232
     """
-    # Get the version number
-    # TODO if modify line after fixing issue with atp for version
-    if not version:
-        exe = atp.get_mechanical_path()  # check for saved mechanical path
-        if exe:
-            version = atp.version_from_path("mechanical", exe)  # version is already int here
-        else:
-            exe, _version = atp.find_mechanical()
-            version = int(_version * 10)
-    else:
-        exe, _version = atp.find_mechanical(version=version)
-        version = int(_version * 10)
+    # checks for saved mechanical path else try to find installation path
+    exe = atp.get_mechanical_path(version)
+    if exe:
+        version = atp.version_from_path("mechanical", exe)
 
     aisol_path = os.path.dirname(exe)
     print(version, aisol_path)
